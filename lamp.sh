c2a355e946815c9719795ba4aba830f0e0746ab8
muptezelinsan <69256318+muptezelinsan@users.noreply.github.com>
2022-05-27 06:26:13 +0300

    Update lamp.sh

--- lamp.sh
+++ lamp.sh
@@ -1,30 +1,46 @@
 #! /bin/bash
 
+if [[ `id -u` != 0 ]]; then
+    echo "Komut dosyasini calistirmak için ROOT olmalisiniz"
+    exit
+fi
+
 echo "apache/httpd kurulumu"
 
 # apache/httpd kurulumu
-sudo pacman -Syu
-sudo pacman -Syu apache 
+pacman -Syu
+pacman -Syu apache 
 
-# apache/httpd konfigürasyonu
-sudo systemctl enable --now httpd
-sudo systemctl start httpd
-sed -i 's/#unique_id_module/unique_id_module/g' /etc/httpd/conf/httpd.conf 
-sudo systemctl restart httpd
+# apache/httpd konfigurasyonu
+systemctl enable --now httpd
+systemctl start httpd
+sed -i 's/#LoadModule unique_id_module/LoadModule unique_id_module/g' /etc/httpd/conf/httpd.conf
+systemctl restart httpd
+
+cat << EOF > /srv/http/index.html
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <title>Welcome</title>
+ </head>
+<body>
+  <h2>Welcome to my Web Server test page</h2>
+</body>
+</html>
+EOF
 
 clear
 
 echo "Php Kurulumu"
 
 # Php Kurulumu
-sudo pacman -S php php-cgi php-gd php-pgsql php-apache
-sed -i 's/mpm_event_module/#mpm_event_module/g' /etc/httpd/conf/httpd.conf
-sed -i 's/#mpm_prefork_module/mpm_prefork_module/g' /etc/httpd/conf/httpd.conf
-
-phpchose() {
+phpvrs() {
         while :
         do
-                read -p 'Lütfen php sürümünüzü seçin -->  (php7/php8?): ' answer
+                read -p 'Yuklemek istediginiz php surumunu secin -->  ( php7 / php8 ?): ' answer
                 case "${answer}" in
                     [7]|[php7][7.0]]) exit 0 ;;
                     [8]|[php87][8.0]]) exit 1 ;;
@@ -33,7 +49,9 @@ phpchose() {
 }
 
 
-if $( phpchose ); then
+if $( phpvrs ); then
+pacman -S php7 php7-cgi php7-gd php7-pgsql php7-apache
+
 cat << EOF >> /etc/httpd/conf/httpd.conf
     LoadModule php7_module modules/libphp7.so
     AddHandler php7-script php
@@ -41,7 +59,10 @@ cat << EOF >> /etc/httpd/conf/httpd.conf
 EOF
 sed -i 's/;gd/gd/g' /etc/php7/php.ini
 sed -i 's/;mysqli/mysqli/g' /etc/php7/php.ini
+
 else
+pacman -S php php-cgi php-gd php-pgsql php-apache
+
 cat << EOF >> /etc/httpd/conf/httpd.conf
     LoadModule php_module modules/libphp7.so
     AddHandler php-script php
@@ -51,57 +72,38 @@ sed -i 's/;gd/gd/g' /etc/php/php.ini
 sed -i 's/;mysqli/mysqli/g' /etc/php/php.ini
 fi
 
+sed -i 's/LoadModule mpm_event_module/#LoadModule mpm_event_module/g' /etc/httpd/conf/httpd.conf
+sed -i 's/#LoadModule mpm_prefork_module/LoadModule mpm_prefork_module/g' /etc/httpd/conf/httpd.conf
+
 cat << EOF > /srv/http/info.php
 <?php phpinfo();
 EOF
 
-sudo systemctl restart httpd
-
+systemctl restart httpd
 
 clear
 
 echo "MySQL/MariaDb Kurulumu"
 
 # MySQL/MariaDb Kurulumu
-sudo pacman -S mariadb
-sudo mysql_install_db — user=mysql — basedir=/usr — datadir=/var/lib/mysql
-sudo systemctl enable mariadb
-sudo systemctl start mariadb
-sudo mysql_secure_installation
+pacman -S mariadb
+mysql_install_db — user=mysql — basedir=/usr — datadir=/var/lib/mysql
+systemctl enable mariadb
+systemctl start mariadb
+mysql_secure_installation
 
-echo "MySQL, root şifresini sorduğunda “Enter” düğmesine basın.  Kullanıcıdan “Kök şifresi ayarla mı?” diye soracaktır.  Şifreyi ayarlamak için 'Y' tuşunu yazın ve enter'a basın.  Her ikisi de farklı şeyler olduğundan, root şifresinin sunucu şifresi ile karıştırılmaması gerektiğini unutmayın.
+echo "MySQL, root sifresini sordugunda 'Enter' dugmesine basin.  Kullanicidan 'Kok sifresi ayarla mi?' diye soracaktir.  sifreyi ayarlamak icin 'Y' tusunu yazin ve enter'a basin.  Her ikisi de farkli seyler oldugundan, root sifresinin sunucu sifresi ile karistirilmamasi gerektigini unutmayin.
 
-Bittiğinde, MySQL kurulumunu tamamlamak için tüm seçeneklere 'Y' girin.  MySQL, değişiklikleri otomatik olarak yeniden yükleyecek ve yükleyecektir."
-sleep 3s
-echo "5 Saniye içerisinde işleme yönlendirileceksiniz"
-echo "4 Saniye içerisinde işleme yönlendirileceksiniz"
-echo "3 Saniye içerisinde işleme yönlendirileceksiniz"
-echo "2 Saniye içerisinde işleme yönlendirileceksiniz"
-echo "1 Saniye içerisinde işleme yönlendirileceksiniz"
+Bittiginde, MySQL kurulumunu tamamlamak icin tum seceneklere 'Y' girin.  MySQL, degisiklikleri otomatik olarak yeniden yukleyecek ve yukleyecektir."
+sleep 5s
+echo "5 Saniye icerisinde isleme yonlendirileceksiniz"
+echo "4 Saniye icerisinde isleme yonlendirileceksiniz"
+echo "3 Saniye icerisinde isleme yonlendirileceksiniz"
+echo "2 Saniye icerisinde isleme yonlendirileceksiniz"
+echo "1 Saniye icerisinde isleme yonlendirileceksiniz"
 
 clear
 
-echo "Otomatik yükleme başlatılıyor"
+echo "Otomatik yukleme baslatiliyor"
 
-echo "Kurulum Tamamlandı."
-
-localTest() {
-        while :
-        do
-                read -p 'localhost sayfsını test et..! -->  (evet/hayır?): ' answer
-                case "${answer}" in
-                    [eE][evet][Evet][yY]|[yes][Yes]]) exit 0 ;;
-                    [hH][hayır][Hayır][nN]|[no][No]]) exit 1 ;;
-                esac
-        done
-}
-
-
-if $( localTest ); then
-    firefox-developer-edition https://localhost/
-else
-    exit
-fi
-
-
-
+echo "Kurulum Tamamlandi."
