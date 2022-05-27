c8567a3b2e365569799116f3da124b69c3d9e551
muptezelinsan <69256318+muptezelinsan@users.noreply.github.com>
2022-05-27 21:14:17 +0300

    Update and rename empty.sh to lamp-stack.sh

--- lamp-stack.sh
+++ lamp-stack.sh
@@ -1,53 +1,55 @@
 #!/bin/bash
 
 ############################################################################
-########################### LAMP configuration #############################
+########################### LAMP SERVER KURULUMU ###########################
 ############################################################################
 
 apache_install() {
 
-	echo -e "\e[32m Installl Apache..."
+	echo -e "\e[32m Apache yukleniyor..."
 	printf "\n"
 	sudo pacman -Sy apache --noconfirm
 	sudo sed -i 's/LoadModule unique_id_module modules\/mod_unique_id.so/#LoadModule unique_id_module modules\/mod_unique_id.so/' /etc/httpd/conf/httpd.conf
 	printf "\n"
-	echo -e "\e[34m Enabling apache service at boot..."
+	echo -e "\e[34m Apache hizmeti etkinlestiriliyor..."
 	printf "\n"
 	sudo systemctl enable httpd
 	sudo systemctl start httpd
 	printf "\n"
-	echo -e "\e[92m Apache installed successfully!"
+	echo -e "\e[92m Apache yuklemesi tamamlandi..!"
 	printf "\n"
+    sleep 3
 }
 
 mariadb_install() {
-	echo -e "\e[32m Installing Mariadb..."
+	echo -e "\e[32m Mariadb yukleniyor..."
 	printf "\n"
 	sudo pacman -S mariadb --noconfirm
 	printf "\n"
-	echo -e "\e[34m Initializing Mariadb data directory..."
+	echo -e "\e[34m Mariadb veri dizini baslatiliyor..."
 	printf "\n"
 	sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 	printf "\n"
-	echo -e "\e[34m Enabling Mariadb service at boot..."
+	echo -e "\e[34m Mariadb hizmeti etkinlestiriliyor..."
 	printf "\n"
 	sudo systemctl enable mariadb.service
 	sudo systemctl start mariadb.service
 	printf "\n"
-	echo -e "\e[33m Now carefully configure mariadb database security according to your choice..."
+	echo -e "\e[33m Mariadb veritabani guvenligi secenekleri... Lutfen dikkatlice yapilandirin..."
 	printf "\n"
 	sudo /usr/bin/mysql_secure_installation
 	printf "\n"
-	echo -e "\e[92m Mariadb installed successfully!"
+	echo -e "\e[92m Mariadb yuklemesi tamamlandi..!"
 	printf "\n"
+    sleep 3
 }
 
 php_install() {
-	echo -e "\e[32m Installing php..."
+	echo -e "\e[32m Php yukleniyor..."
 	printf "\n"
 	sudo pacman -S php php-apache --noconfirm
 	printf "\n"
-	echo -e "\e[34m Configuring php modules..."
+	echo -e "\e[34m Php modulleri yapilandiriliyor..."
 	printf "\n"
 	sudo sed -i 's/;extension=bz2.so/extension=bz2.so/' /etc/php/php.ini
 	sudo sed -i 's/;extension=mcrypt.so/extension=mcrypt.so/' /etc/php/php.ini
@@ -57,22 +59,23 @@ php_install() {
 	sudo sed -i 's/;extension=zip.so/extension=zip.so/' /etc/php/php.ini
 	sudo sed -i 's/LoadModule mpm_event_module modules\/mod_mpm_event.so/#LoadModule mpm_event_module modules\/mod_mpm_event.so/'  /etc/httpd/conf/httpd.conf
 	sudo sed -i 's/^#LoadModule mpm_prefork_module modules\/mod_mpm_prefork.so/LoadModule mpm_prefork_module modules\/mod_mpm_prefork.so/' /etc/httpd/conf/httpd.conf
-	sudo sed -i '68 i\LoadModule php7_module modules/libphp7.so' /etc/httpd/conf/httpd.conf
-	sudo sed -i '69 i\AddHandler php7-script php' /etc/httpd/conf/httpd.conf
-	sudo sed -i '70 i\Include conf/extra/php7_module.conf' /etc/httpd/conf/httpd.conf
+	sudo sed -i '68 i\LoadModule php_module modules/libphp.so' /etc/httpd/conf/httpd.conf
+	sudo sed -i '69 i\AddHandler php-script php' /etc/httpd/conf/httpd.conf
+	sudo sed -i '70 i\Include conf/extra/php_module.conf' /etc/httpd/conf/httpd.conf
 	printf "\n"
 	sudo systemctl restart httpd
 	printf "\n"
-	echo -e "\e[92m php installed successfully!"
+	echo -e "\e[92m Php yuklemesi tamamlandi..!"
 	printf "\n"
+    sleep 3
 }
 
 phpmyadmin_install() {
-	echo -e "\e[32m Installing phpmyadmin..."
+	echo -e "\e[32m phpMyAdmin yukleniyor..."
 	printf "\n"
 	sudo pacman -S php-mcrypt phpmyadmin --noconfirm
 	printf "\n"
-	echo -e "\e[34m Configuring phpmyadmin..."
+	echo -e "\e[34m phpMyAdmin modulleri yapilandiriliyor..."
 	printf "\n"
 	sudo touch /etc/httpd/conf/extra/phpmyadmin.conf
 	sudo echo 'Alias /phpmyadmin "/usr/share/webapps/phpMyAdmin"' > /etc/httpd/conf/extra/phpmyadmin.conf
@@ -90,83 +93,46 @@ phpmyadmin_install() {
 	printf "\n"
 	sudo systemctl restart httpd
 	printf "\n"
-	echo -e "\e[92m phpMyAdmin installed successfully!"
+	echo -e "\e[92m phpMyAdmin yuklemesi tamamlandi..!"
 	printf "\n"
-	echo -e "\e[92m phpMyAdmin status!"
+    sleep 3
+	echo -e "\e[92m phpMyAdmin hizmeti durumu..!"
 	sudo systemctl status phpMyAdmin
-
 	printf "\n"
+    sleep 3
 }
 
 finalize() {
-	echo -e "\e[36m Finalizing..."
+	echo -e "\e[36m Konfigurasyonlar tanimlaniyor..."
 	printf "\n"
 	sudo cp -f httpd.conf /etc/httpd/conf/httpd.conf
 	sudo cp -f php.ini /etc/php/php.ini
 	sudo cp -f phpmyadmin.conf /etc/httpd/conf/extra/phpmyadmin.conf
 	sudo cp -f config.inc.php /etc/webapps/phpmyadmin/config.inc.php
 	printf "\n"
-	echo -e "\e[92m Configured successfully!"
+	echo -e "\e[92m Konfigurasyonlar basarili bir sekilde tanimlandi..!"
+    sleep 3
 }
 
  certbot-apache () {
-
-  echo -e "Installing certbot-apache"
+  echo -e "certbot-apache kurulumu"
 	printf "\n"
 	sudo trize -Sy -certbot-apache --noconfirm
 	sudo certbot --apache
-	echo -e "\e[34m Enabling -certbot-apache servie at boot..."
+	echo -e "\e[34m certbot-apache hizmeti etkinlestiriliyor..."
 	printf "\n"
 	sudo systemctl enable certbot-apache
 	sudo systemctl start certbot-apache
 	sudo sed -i 'Include conf/extra/httpd-acme.conf' /etc/httpd/conf/httpd.conf
 	printf "\n"
-	echo -e "\e[92m Apache installed successfully!"
+	echo -e "\e[92m certbot-Apache yuklemesi tamamlandi..!"
 	printf "\n"
-
+    sleep 3
  }
 
- ### 5.1 PostgreSQL
-+
-+Install Postgresql 9.1:
-+
-+sud mkdir /tmp/postgresql
-+    cd /tmp/postgresql
-+    wget -o https://aur.archlinux.org/packages/po/postgresql-9.1/postgresql-9.1.tar.gz
-+    makepkg --asroot
-+   sudo  pacman -U postgres*.tar.xz
-+
-+    # If you get this message, accept the 'yes' resolution.
-+    # :: postgresql and postgresql-libs are in conflict. Remove postgresql-libs? [y/N] y
-+
-+Initialize the database:
-+
-+   sudo mkdir /var/lib/postgres
-+    sudo chown -R postgres:postgres /var/lib/postgres
-+   sudo chmod -R 700 /var/lib/postgres
-+    su - postgres initdb --locale en_US.UTF-8 -E UTF8 -D '/var/lib/postgres/data'
-+    # return to the root user (from postgres user)
-+    logout
-+    systemctl start postgresql
-+    systemctl enable postgresql
-+
-+Configure the database user and password:
-+
-+    su - postgres
-+    psql -d template1
-+    # psql (9.1.13)
-+
-+    template1=# CREATE USER git WITH PASSWORD 'your-password-here';
-+    CREATE ROLE
-+    template1=# CREATE DATABASE gitlabhq_production OWNER git;
-+    CREATE DATABASE
-+    template1=# \q
-+
-+    # return to root user (from postgres user)
-+    logout
 
 clear
-echo -e "\e[36m Now installing the LAMP server..."
+echo -e "\e[36m Lamp yigini yukleniyor..."
 printf "\n"
 printf "\n"
 apache_install
@@ -179,6 +145,5 @@ phpmyadmin_install
 printf "\n"
 finalize
 printf "\n"
-echo -e "\e[33m phpMyAdmin installed successfully!"
-sleep 2
+sleep 3
 exit
